4bc4fd14ac5a80be0d6630fd4597612a939f79d4
HP <aqa.taher3@gmail.com>
2024-12-02 16:59:45 +0330

    Auto commit on 2024-12-02 16:59:45

---
+++ scheduler.py
@@ -0,0 +1,30 @@
+import os
+import subprocess
+from datetime import datetime
+
+# مسیر پوشه گیت
+GIT_REPO_PATH = r"C:\Users\HP\Documents\exel_010"
+
+
+# پیام کامیت
+commit_message = f"Auto commit on {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
+
+try:
+    os.chdir(GIT_REPO_PATH)
+
+    # افزودن تغییرات
+    subprocess.run(["git", "add", "."], check=True)
+
+    # کامیت کردن
+    subprocess.run(["git", "commit", "-m", commit_message], check=True)
+
+    # پوش کردن
+    subprocess.run(["git", "push"], check=True)
+
+    print("Changes pushed to GitHub successfully.")
+except subprocess.CalledProcessError as e:
+    print(f"An error occurred: {e}")
+
+import os
+username = os.getlogin()
+print(f"Username: {username}")
